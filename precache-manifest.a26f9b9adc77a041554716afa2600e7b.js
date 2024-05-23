05c3fc03d1599a73b8630413c259a9435fe971ec
Martin Salomon <salomonmartin21@gmail.com>
2024-05-23 13:02:36 -0300

    Updates

---
+++ precache-manifest.a26f9b9adc77a041554716afa2600e7b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ef9317822aacc9ac9e242241584713",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "dc5e54d662ecfd36ec26",
+    "url": "/todoApp/static/css/main.eec40c09.chunk.css"
+  },
+  {
+    "revision": "2db1460efde752c0fd8a",
+    "url": "/todoApp/static/js/2.ae919666.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/todoApp/static/js/2.ae919666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc5e54d662ecfd36ec26",
+    "url": "/todoApp/static/js/main.0a389721.chunk.js"
+  },
+  {
+    "revision": "76f25c3c9258c3e86e68",
+    "url": "/todoApp/static/js/runtime-main.cb301c9b.js"
+  },
+  {
+    "revision": "02ca93ed3fd68013933c9dadabeefdf0",
+    "url": "/todoApp/static/media/icon-ellipsis.02ca93ed.svg"
+  },
+  {
+    "revision": "1f2aa98ef1bebee3fa976340de755146",
+    "url": "/todoApp/static/media/icon-exercise.1f2aa98e.svg"
+  },
+  {
+    "revision": "1a5250a3891fdf036f44c0141a2407d9",
+    "url": "/todoApp/static/media/icon-play.1a5250a3.svg"
+  },
+  {
+    "revision": "78d6a4ccd44b9c9c8a8512f9bbbb70ee",
+    "url": "/todoApp/static/media/icon-self-care.78d6a4cc.svg"
+  },
+  {
+    "revision": "0572c3afee42a4eaaa86e6e785af5339",
+    "url": "/todoApp/static/media/icon-social.0572c3af.svg"
+  },
+  {
+    "revision": "267a0ddfeba20d0ffb9ac1c4e2b58aca",
+    "url": "/todoApp/static/media/icon-study.267a0ddf.svg"
+  },
+  {
+    "revision": "b166cda623a8a7acdde0ffd51667229c",
+    "url": "/todoApp/static/media/icon-work.b166cda6.svg"
+  }
+]);
